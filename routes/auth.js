bfab557399b49475da036401f12cda260327c2af
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-02 01:39:45 +0530

    login system

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,12 @@ const bcrypt = require('bcrypt');
 // REGISTER USER
 router.post("/register", async (req,res) => {
     try {
+        // if user already exists
+        const user = await User.findOne({ email: req.body.email });
+        if(user) {
+            return res.status(401).json({ msg: "User Already Exists" });
+        }
+
         // encrpyt user password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -20,7 +26,24 @@ router.post("/register", async (req,res) => {
         const user = await newUser.save();
         res.status(200).json(user)
     } catch(err) {
-        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// LOGIN 
+router.post("/login", async (req,res) => {
+    try {
+        // if user doesn't exists
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("User Not Found");
+
+        // if user enters incorrect password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("Incorrect Password");
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
