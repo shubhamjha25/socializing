f0a8b4da108aa01e43de8e400f1a87be5f9d2dc9
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-08 00:31:19 +0530

    get user

--- routes/users.js
+++ routes/users.js
@@ -46,4 +46,16 @@ router.delete("/:id", async (req,res) => {
     }
 });
 
+// GET USER
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
