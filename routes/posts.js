3d1ac45ea7430ab7cc34291e0d3afa1ff61daa32
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-15 12:02:02 +0530

    create post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+const User = require("../models/User");
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-    res.send("Posts Page");
+// CREATE A POST
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        // save post in the database
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
