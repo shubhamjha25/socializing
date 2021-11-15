41455048ae7c9d95bb76b8d46c9219e0cbf3a443
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-15 12:02:43 +0530

    update post

--- routes/posts.js
+++ routes/posts.js
@@ -14,4 +14,21 @@ router.post("/", async (req, res) => {
     }
 });
 
+// UPDATE A POST
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        // userId's should be matched
+        if(post.userId === req.body.userId) {
+            await post.updateOne({$set: req.body});
+            res.status(200).json("Post Updated Successfully");
+        }
+        else {
+            res.status(403).json("Access Denied - You cannot update other user's post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
