79a3f39aebd5296a657a9c990ccdc93a49277bae
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-17 20:54:34 +0530

    get post

--- routes/posts.js
+++ routes/posts.js
@@ -48,4 +48,14 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
+// GET A POST
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
