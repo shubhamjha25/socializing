8cf3b6a689367064d2161030639ded17b1df39fa
Shubham Jha <shubham.jha2001m@gmail.com>
2021-11-18 21:50:03 +0530

    like/dislike a post

--- routes/posts.js
+++ routes/posts.js
@@ -58,4 +58,21 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// LIKE | DISLIKE A POST
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("Post Has Been Liked");
+        } 
+        else {
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("Post Has Been Disliked");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
